a5b429d08b368f6141f9c32df51d53b772a753f2
Rodrigo <rodrigolemosrl@hotmail.com>
2021-04-09 18:04:57 -0300

    build: change service port

--- service/src/app.ts
+++ service/src/app.ts
@@ -3,6 +3,6 @@ import app from './server';
 
 getServerConnection();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`App running on port ${port}`));
