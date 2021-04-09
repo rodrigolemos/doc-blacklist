11f4f3330ad95a41ea5d4e51876f486179e11372
Rodrigo <rodrigolemosrl@hotmail.com>
2021-04-09 10:42:47 -0300

    feat(service): handle errors and connect to database

---
+++ service/src/errors/AppError.ts
@@ -0,0 +1,12 @@
+class AppError {
+  public readonly message: string | object;
+
+  public readonly statusCode: number;
+
+  constructor(message: string | object, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default AppError;
