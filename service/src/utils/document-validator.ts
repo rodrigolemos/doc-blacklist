c5e22e8e70359ab87d193e67b2d8b16f78476497
Rodrigo <rodrigolemosrl@hotmail.com>
2021-04-09 16:37:28 -0300

    feat(service): check if cpf or cnpj is valid

--- service/src/utils/document-validator.ts
+++ service/src/utils/document-validator.ts
@@ -1,4 +1,71 @@
-export const validateCPF = (strCPF: string): boolean => {
+export const validateDocument = (strDocument: string): { type: string, isValid: boolean } => {
+  if (strDocument.length > 11) {
+    return {
+      type: 'C',
+      isValid: validateCNPJ(strDocument)
+    };
+  }
+  return {
+    type: 'P',
+    isValid: validateCPF(strDocument)
+  };
+}
+
+const validateCNPJ = (strCNPJ: string): boolean => {
+  if (!strCNPJ) {
+    return false;
+  }
+
+  const validTypes = typeof strCNPJ === 'string' || Number.isInteger(strCNPJ) || Array.isArray(strCNPJ);
+
+  if (!validTypes) {
+    return false;
+  }
+
+  const match = strCNPJ.toString().match(/\d/g);
+  const numbers = Array.isArray(match) ? match.map(Number) : [];
+
+  if (numbers.length !== 14) {
+    return false;
+  }
+  
+  const items = [...new Set(numbers)];
+  if (items.length === 1) {
+    return false;
+  }
+
+  const calc = (x: number) => {
+    const slice = numbers.slice(0, x);
+    let factor = x - 7;
+    let sum = 0;
+
+    for (let i = x; i >= 1; i--) {
+      const n = slice[x - i];
+      sum += n * factor--;
+      if (factor < 2) {
+        factor = 9;
+      }
+    }
+
+    const result = 11 - (sum % 11);
+
+    return result > 9 ? 0 : result;
+  }
+
+  const digits = numbers.slice(12);
+  
+  const digit0 = calc(12);
+
+  if (digit0 !== digits[0]) {
+    return false;
+  }
+
+  const digit1 = calc(13);
+
+  return (digit1 === digits[1])
+}
+
+const validateCPF = (strCPF: string): boolean => {
   
   let sum = 0;
   let rest;
