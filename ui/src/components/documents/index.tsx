988e206a88ee585b70e04c274a326e26a555b2f2
Rodrigo <rodrigolemosrl@hotmail.com>
2021-04-10 16:16:42 -0300

    feat(ui): add chakra ui form

--- ui/src/components/documents/index.tsx
+++ ui/src/components/documents/index.tsx
@@ -1,14 +1,49 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { Container, Content, Filters } from './styles';
 
+import { Button, Input, Radio, RadioGroup, Select, Stack } from "@chakra-ui/react";
+
 const Documents: React.FC = (): ReactElement => {
+  const [type, setType] = useState<string>('');
+  const [blacklist, setBlacklist] = useState<string>('');
+  const [value, setValue] = useState<string>('');
+
   return (
     <Container>
       <Content>
+
         <Filters>
-          <span>Filters</span>
+          <Stack spacing={15} direction="row" align="center" w="90%">
+            <RadioGroup onChange={setType} value={type} mr={10}>
+              <Stack direction="row">
+                <Radio value="">Todos</Radio>
+                <Radio value="P">CPF</Radio>
+                <Radio value="C">CNPJ</Radio>
+              </Stack>
+            </RadioGroup>
+
+            <Select onChange={e => { setBlacklist(e.target.value)}} value={blacklist}>
+              <option value="">Todos os documentos</option>
+              <option value="Y">Somente os que são blacklist</option>
+              <option value="N">Somente os que NÃO são blacklist</option>
+            </Select>
+
+            <Input
+              placeholder="Pesquise um documento..."
+              minLength={11}
+              maxLength={14}
+              value={value}
+              onChange={(e) => { setValue(e.target.value)}}
+            />
+          </Stack>
+
+          <Stack direction="row" align="center">
+            <Button colorScheme="blue">Pesquisar</Button>
+          </Stack>
         </Filters>
-        <span>Table</span>
+
+        {/* <span>Table</span> */}
+
       </Content>
     </Container>
   );
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalStyle from './GlobalStyle';
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <GlobalStyle />
+    <ChakraProvider>
+      <App />
+      <GlobalStyle />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
